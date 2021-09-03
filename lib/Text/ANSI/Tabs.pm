66fe0b89536aab14422cadfd9f9f9ae56cea5ab3
Kazumasa Utashiro <kaz@utashiro.com>
2021-09-03 18:39:40 +0900

    minil new Text::ANSI::Tabs

---
+++ lib/Text/ANSI/Tabs.pm
@@ -0,0 +1,39 @@
+package Text::ANSI::Tabs;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Text::ANSI::Tabs - It's new $module
+
+=head1 SYNOPSIS
+
+    use Text::ANSI::Tabs;
+
+=head1 DESCRIPTION
+
+Text::ANSI::Tabs is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
