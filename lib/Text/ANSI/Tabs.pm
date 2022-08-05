b5729aa14fed132d6f03966d0fd567a2fa0f1da4
Kazumasa Utashiro <kaz@utashiro.com>
2022-08-06 06:50:27 +0900

    Checking in changes prior to tagging of version 0.08.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 7e8f615..54feb0c 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Text-ANSI-Tabs
     
     {{$NEXT}}
     
    +0.08 2022-08-05T21:50:24Z
    +
    +    - require Text::ANSI::Fold@2.1301
    +
     0.07 2021-09-04T08:00:32Z
     
         - Use local $fold object and provide configure().

--- lib/Text/ANSI/Tabs.pm
+++ lib/Text/ANSI/Tabs.pm
@@ -1,5 +1,5 @@
 package Text::ANSI::Tabs;
-our $VERSION = "0.07";
+our $VERSION = "0.08";
 
 =encoding utf-8
 
@@ -20,7 +20,7 @@ Text::ANSI::Tabs - Tab expand and unexpand with ANSI sequence
 
 =head1 VERSION
 
-Version 0.07
+Version 0.08
 
 =cut
 
